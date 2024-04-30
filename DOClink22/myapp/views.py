809ac409447980c3b9d9851aeee1bc83705c7911
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2024-04-30 14:42:53 -0700

    Add files via upload

---
+++ DOClink22/myapp/views.py
@@ -0,0 +1,163 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+
+from .models import Specialization, User, Clinic, Location, Doctor, Appointment, Patient, Hospital, Bed, ClinicSchedule, Notification, Rooms, Assistent,Chatbox
+from .serializer import SpecializationSerializer,UserSerializer, ClinicSerializer, LocationSerializer, DoctorSerializer, AppointmentSerializer, PatientSerializer, HospitalSerializer, BedSerializer, ClinicScheduleSerializer, NotificationSerializer, RoomsSerializer, AssistentSerializer, ChatboxSerializer
+
+
+class SpecializationListView(generics.ListCreateAPIView):
+    queryset = Specialization.objects.all()
+    serializer_class = SpecializationSerializer
+  
+
+class SpecializationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Specialization.objects.all()
+    serializer_class = SpecializationSerializer
+
+class RoomsListView(generics.ListCreateAPIView):
+    queryset = Rooms.objects.all()
+    serializer_class = RoomsSerializer
+  
+
+class RoomsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Rooms.objects.all()
+    serializer_class = RoomsSerializer
+
+class AssistentListView(generics.ListCreateAPIView):
+    queryset = Assistent.objects.all()
+    serializer_class = AssistentSerializer
+  
+
+class AssistentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Assistent.objects.all()
+    serializer_class = AssistentSerializer
+
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ClinicListView(generics.ListCreateAPIView):
+    queryset = Clinic.objects.all()
+    serializer_class = ClinicSerializer
+  
+
+class ClinicDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Clinic.objects.all()
+    serializer_class = ClinicSerializer
+    
+
+
+class LocationListView(generics.ListCreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    
+
+class LocationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    
+
+class DoctorListView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        doctor_specialization = serializer.validated_data.get("specialization")
+        doctor_location = serializer.validated_data.get("location")
+        serializer.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+class DoctorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    
+
+class BedListView(generics.ListCreateAPIView):
+    queryset = Bed.objects.all()
+    serializer_class = BedSerializer
+
+
+class BedDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bed.objects.all()
+    serializer_class = BedSerializer
+  
+class PatientListView(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        # Ensure location is provided before saving
+        if 'location' not in serializer.validated_data:
+            return Response({'error': 'Location is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+
+class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+  
+
+class AppointmentListView(generics.ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    
+
+class AppointmentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+
+class HospitalListView(generics.ListCreateAPIView):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
+
+
+class HospitalDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
+  
+
+class ClinicScheduleListView(generics.ListCreateAPIView):
+    queryset = ClinicSchedule.objects.all()
+    serializer_class = ClinicScheduleSerializer
+  
+
+class ClinicScheduleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClinicSchedule.objects.all()
+    serializer_class = ClinicScheduleSerializer
+  
+
+class NotificationListView(generics.ListCreateAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+class ChatboxListView(generics.ListCreateAPIView):
+    queryset = Chatbox.objects.all()
+    serializer_class = ChatboxSerializer
+
+class ChatboxDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chatbox.objects.all()
+    serializer_class = ChatboxSerializer
+
+
+
